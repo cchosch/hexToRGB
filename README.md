664299606e3512320ce644ad510756aa57e59ec8
cchosch <colinhoscheit@gmail.com>
2022-09-18 13:28:48 -0400

    all running

---
+++ README.md
@@ -0,0 +1,6 @@
+# hexToRGB
+hexToRGB takes any hex value and gives an array containing the three rgb values
+
+# Usage
+Run `python3 main.py` and enter your hex value
+
---
+++ main.py
@@ -0,0 +1,22 @@
+
+running = True
+try:
+    while running:
+        hex_val = input("")
+        if hex_val == "exit" or hex_val == "q" or hex_val == "quit":
+            running = False
+            break
+
+        hex_val = hex_val.removeprefix("#")
+        if len(hex_val) != 6:
+            print("Please enter valid input")
+            continue 
+
+        hex_vals = [int(hex_val[0:2], 16), int(hex_val[2:4], 16), int(hex_val[4:6], 16)]
+        print(str(hex_vals)+",")
+except KeyboardInterrupt:
+    pass
+
+
+
+
