d76ba7638a828c13501dc08bf8c1cba655237838
cchosch <colinhoscheit@gmail.com>
2022-09-18 13:30:15 -0400

    clear output when ctrl+c

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ try:
         hex_vals = [int(hex_val[0:2], 16), int(hex_val[2:4], 16), int(hex_val[4:6], 16)]
         print(str(hex_vals)+",")
 except KeyboardInterrupt:
-    pass
+    print("\r                                                   ",end="")
 
 
 
